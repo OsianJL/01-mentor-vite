ce98d950602c6088df3e31a3442357f666441f8b
Osián <149536063+OsianJL@users.noreply.github.com>
2024-11-01 09:04:36 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'mentor-orange': 'hsl(35, 77%, 62%)',
+        
+
+      }
+    },
+  },
+  plugins: [],
+}
